58d55d9ff074a26f67c83a83646b9b102159b4f0
Steven Stone <srstone@ucsd.edu>
2018-07-17 17:19:30 -0700

    Changed Node class in Link List

--- project/code/linklist/Node.cpp
+++ project/code/linklist/Node.cpp
@@ -6,6 +6,27 @@
  *
  **/
 
+class Node
+{
+  int data;
+  Node *prev, *next;
+
+  //Constructor
+  Node (int data)
+  {
+    this->data = data;
+    prev = 0;
+    next = 0;
+  }
+
+
+
+
+};
+
+
+
+
 int main()
 {
   return 0;
